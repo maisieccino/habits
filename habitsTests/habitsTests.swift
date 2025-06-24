53b38f88353ea556fd14e0c66b89833f7dbef13d
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2025-06-24 14:52:35 +0100

    Initial Commit

---
+++ habitsTests/habitsTests.swift
@@ -0,0 +1,16 @@
+//
+//  habitsTests.swift
+//  habitsTests
+//
+//  Created by Maisie Bell on 24/06/2025.
+//
+
+import Testing
+
+struct habitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
